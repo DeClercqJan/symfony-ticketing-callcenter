e87311291d59fe348c7b5ef1fa7a433fcb295a3e
jan <declercqjan@gmail.com>
2020-04-24 15:16:21 +0200

    basic password restore functionality

--- src/Controller/TicketController.php
+++ src/Controller/TicketController.php
@@ -34,25 +34,4 @@ class TicketController extends AbstractController
             'ticket' => $ticket,
         ]);
     }
-
-    /**
-     * @Route("/email")
-     */
-    public function sendEmail(MailerInterface $mailer)
-    {
-        $email = (new Email())
-            ->from('hello@example.com')
-            ->to('eddyeddyeddyborremans@gmail.com')
-            //->cc('cc@example.com')
-            //->bcc('bcc@example.com')
-            //->replyTo('fabien@example.com')
-            //->priority(Email::PRIORITY_HIGH)
-            ->subject('Time for Symfony Mailer!')
-            ->text('Sending emails is fun again!')
-            ->html('<p>Hallowkes See Twig integration for better HTML integration!</p>');
-
-        $mailer->send($email);
-
-        dd('great succes!');
-    }
 }
