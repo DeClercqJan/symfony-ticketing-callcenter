f1a771608da905d61ad55462e4d5bd0a0ad41347
jan <declercqjan@gmail.com>
2020-04-24 17:07:23 +0200

    welcome mail check. Also improved routing flows logging in and out and such

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,19 +5,25 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use App\Security\LoginFormAuthenticator;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
 class RegistrationController extends AbstractController
 {
+
     /**
      * @Route("/register", name="app_register")
      */
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $authenticator): Response
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $authenticator, MailerInterface $mailer): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -37,16 +43,26 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
 
             // do anything else you need here, like send an email
+            // reusing code from the symfony cast reset-password bundle thingy
+            $email = (new TemplatedEmail())
+                ->from(new Address('welcome@moshimoshicallcenter.com', 'Welcome Bot'))
+                ->to($user->getEmail())
+                ->subject('Succesful registration')
+                ->html("<")
+                ->htmlTemplate('registration/registration_email.html.twig')
+            ;
 
+            $mailer->send($email);
 
             return $guardHandler->authenticateUserAndHandleSuccess(
                 $user,
                 $request,
                 $authenticator,
                 'main' // firewall name in security.yaml
-            );
+            )
+                ?: new RedirectResponse('/login'); // fallback
+            //;
         }
-
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
