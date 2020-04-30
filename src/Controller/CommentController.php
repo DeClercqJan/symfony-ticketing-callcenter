239dfca906eef47ba0dff7cfd6627a235980171d
Jan <declercqjan@gmail.com>
2020-04-30 14:17:20 +0200

    linked tickets and the possibility to comment on a ticket with various routes, preferring one customer journey, but also allowing an overview of comments. However, the order of pages and how they link may be organised less confusingly

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -3,63 +3,85 @@
 namespace App\Controller;
 
 use App\Entity\Comment;
+use App\Entity\Ticket;
 use App\Form\CommentType;
 use App\Repository\CommentRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/comment")
- */
 class CommentController extends AbstractController
 {
     /**
-     * @Route("/", name="comment_index", methods={"GET"})
+     * @Route("ticket/all/comment", name="comment_index_all", methods={"GET"})
      */
-    public function index(CommentRepository $commentRepository): Response
+    public function indexCommentsAllTickets(CommentRepository $commentRepository): Response
     {
-        return $this->render('comment/index.html.twig', [
+        return $this->render('comment/index_all.html.twig', [
             'comments' => $commentRepository->findAll(),
         ]);
     }
 
     /**
-     * @Route("/new", name="comment_new", methods={"GET","POST"})
+     * @Route("ticket/{ticketid}/comment", name="comment_index_per_ticket", methods={"GET"})
+     * @ParamConverter("ticket", options={"mapping": {"ticketid" : "id"}})
      */
-    public function new(Request $request): Response
+    public function indexCommentsPerTicket(Ticket $ticket, CommentRepository $commentRepository): Response
+    {
+        $comment = $ticket->getComments();
+
+        return $this->render('comment/index_per_ticket.html.twig', [
+            'comments' => $comment,
+            'ticket' => $ticket,
+        ]);
+    }
+
+    /**
+     * @Route("/ticket/{ticketid}/comment/new", name="comment_new", methods={"GET","POST"})
+     * @ParamConverter("ticket", options={"mapping": {"ticketid" : "id"}})
+     */
+    public function new(Ticket $ticket, Request $request): Response
     {
         $comment = new Comment();
         $form = $this->createForm(CommentType::class, $comment);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setTicket($ticket);
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($comment);
             $entityManager->flush();
 
-            return $this->redirectToRoute('comment_index');
+            $ticketid = $ticket->getId();
+            return $this->redirectToRoute('ticket_show', array('id' => $ticketid));
         }
 
         return $this->render('comment/new.html.twig', [
             'comment' => $comment,
             'form' => $form->createView(),
+            'ticket' => $ticket
         ]);
     }
 
     /**
-     * @Route("/{id}", name="comment_show", methods={"GET"})
+     * @Route("/ticket/{ticketid}/comment/{commentid}", name="comment_show", methods={"GET"})
+     * @ParamConverter("ticket", options={"mapping": {"ticketid" : "id"}})
+     * @ParamConverter("comment", options={"mapping": {"commentid" : "id"}})
      */
-    public function show(Comment $comment): Response
+    public function show(Ticket $ticket, Comment $comment): Response
     {
+//        dd($ticket);
         return $this->render('comment/show.html.twig', [
             'comment' => $comment,
         ]);
     }
 
     /**
-     * @Route("/{id}/edit", name="comment_edit", methods={"GET","POST"})
+     * @Route("/ticket/{ticketid}/comment/{commentid}/edit", name="comment_edit", methods={"GET","POST"})
+     * @ParamConverter("ticket", options={"mapping": {"ticketid" : "id"}})
+     * @ParamConverter("comment", options={"mapping": {"commentid" : "id"}})
      */
     public function edit(Request $request, Comment $comment): Response
     {
--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -3,12 +3,21 @@
 namespace App\Form;
 
 use App\Entity\Comment;
+use App\Entity\Ticket;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CommentType extends AbstractType
 {
+//    private $ticket;
+//
+//    public function __construct(Ticket $ticket)
+//    {
+//        $this->ticket = $ticket;
+//    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -16,7 +25,10 @@ class CommentType extends AbstractType
             ->add('isCommentPublic')
             ->add('createdAt')
             ->add('updatedAt')
-            ->add('ticket')
+//            ->add('ticket', EntityType::class, [
+//                'class' => Ticket::class,
+////                'empty_data' => $this->ticket,
+//            ])
             ->add('user')
         ;
     }
